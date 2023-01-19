6c808e5d6a2538574053b030b460af09268ddea4
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-01-19 16:19:46 +0900

    TodoList with React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed ");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import styles from "./App.module.css";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((_, todoIndex) => index !== todoIndex));
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!toDo) {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  useEffect(() => {
+    toDos.map((item) => item.key);
+    console.log(toDos);
+  }, [toDos]);
+
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          value={toDo}
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <span key={index} onClick={() => deleteBtn(index)}>
+              ❌
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
